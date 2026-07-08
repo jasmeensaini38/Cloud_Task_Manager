82a04ae7750887f1989ed87027c97faec5eeb4c8
jasmeensaini38 <jasmeensaini226@gmail.com>
2026-07-08 17:17:56 +0530

    Added Professional .gitignore

--- database.py
+++ database.py
@@ -7,14 +7,19 @@ from sqlalchemy.orm import sessionmaker
 # import declarative_base
 from sqlalchemy.orm import declarative_base
 
+# as per day2
+import os
+from dotenv import load_dotenv
+load_dotenv()
+# import the function that can read a .env file,i.e open the .env file and load all its variables 
+# into the memory
 
-
-DATABASE_URL='postgresql+psycopg2://neondb_owner:npg_9mHxM3sVFjGN@ep-late-butterfly-atckuphh-pooler.c-9.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
+DATABASE_URL=os.getenv("DATABASE_URL")
 # here we connecting to a postgresql database hosted on the cloud
 
 # note:click "show Password" and copy the completeconnection string
 
-# create salalchemy engine
+# create salalchemy engineo
 # the engine is responsible for creting fastapi with the cloud PostgreSQL
 
 engine = create_engine(DATABASE_URL)
